98e0bd0dc6daef6d03ee8f59be1d73420fd2c504
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-07-21 20:23:26 +0530

    Added:Extent Report in Script to generate Test Report

--- BookswagonAutomation/Tests/TestCases.cs
+++ BookswagonAutomation/Tests/TestCases.cs
@@ -1,4 +1,6 @@
-﻿using BookswagonAutomation.Pages;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using BookswagonAutomation.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
@@ -9,19 +11,38 @@ namespace BookswagonAutomation.Base
 {
     class TestCases:BaseClass
     {
+        ExtentReports extent= null;
+
+        [OneTimeSetUp]
+        public void ExtentStart()
+        {
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"F:\\VS\\BookswagonAutomation\\BookswagonAutomation\\ExtentReports\\Report.html");
+            extent.AttachReporter(htmlReporter);
+        }
+
         [Test]
         public void BookswagonAutomationTest()
         {
+            ExtentTest test = extent.CreateTest("LoginTest");
             Login login = new Login(driver);
             login.LoginPage();
-        
+
+            ExtentTest test1 = extent.CreateTest("SearchBookTest");
             SearchBook search = new SearchBook(driver);
             search.SearchBookPage();
 
+            ExtentTest test2 = extent.CreateTest("PurchaseBookTest");
             Cart cart = new Cart(driver);
             cart.CartPage();
 
             ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile("F:\\VS\\BookswagonAutomation\\BookswagonAutomation\\TestScreenshot\\PurchaseBook.png", ScreenshotImageFormat.Png);
+        }
+
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            extent.Flush();
         }
     }
 }
